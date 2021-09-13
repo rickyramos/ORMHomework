dbc2f3bbcd11b95733a1487c28ba11a4557929ce
Ricky Ramos <ricky.ramos10@yahoo.com>
2021-09-13 13:32:12 -0500

    Cleaning up the code

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -24,7 +24,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   Category.create(req.body)
-    .then((category) => res.status(400).json(err));
+    .then((category) => res.status(400).json(category));
   // create a new category
 });
 
